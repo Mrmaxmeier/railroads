dcd42d6af787b9a27292f710d4614865cfb93068
felix <felixbauckholt AT gmail PUNKT com>
2013-03-30 22:51:05 +0100

    Char (in stringsFile.py) eingefügt
    eine Methode male(x, y) sollte implementiert werden....

--- stringsFile.py
+++ stringsFile.py
@@ -1,3 +1,45 @@
+import curses
+import copy
+
+black = curses.COLOR_BLACK
+white = curses.COLOR_WHITE
+red = curses.COLOR_RED
+green = curses.COLOR_GREEN
+blue = curses.COLOR_BLUE
+cyan = curses.COLOR_CYAN
+magenta = curses.COLOR_MAGENTA
+yellow = curses.COLOR_YELLOW
+
+class Char:
+	def __init__(self, char):
+		self.char = char
+		
+		self.vitalic = False
+		self.vbold = False
+		self.vunderline = False
+		self.vblink = False
+		
+		self.vtextCol = white
+		self.vbackCol = black
+	
+	def copy(self):
+		return copy.deepcopy(self)
+	
+	def withCopy(self, prop, val):
+		new = self.copy()
+		new.__dict__[prop] = val
+		return new
+	
+	def italic(b=True): self.withCopy("vitalic", b)
+	def bold(b=True): self.withCopy("vbold", b)
+	def underline(b=True): self.withCopy("vunderline", b)
+	def blink(b=True): self.withCopy("vblink", b)
+	
+	def textCol(color): self.withCopy("vtextCol", color)
+	def backCol(color): self.withCopy("vbackCol", color)
+
+
+
 class StringsFile:
 	def __init__(self, name):
 		self.dict = {}
